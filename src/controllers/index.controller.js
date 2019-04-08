f634dc313e03dc90edffb47ec35abf63b3c1015d
Kevin Kim <kevink1103@gmail.com>
2019-04-08 21:22:55 +0800

    recap

--- src/controllers/index.controller.js
+++
@@ -1,7 +0,0 @@
-const index = (req, res, next) => res.json({
-    message: 'Kevin\'s HERE'
-})
-
-export {
-    index
-}
--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import {index} from '../controllers/index.controller'
-
-const router = express.Router()
-
-/*  GET home page */
-router.get('/', index)
-
-export default router
